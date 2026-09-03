e8e9d071081175a3134498a06a3406ff61287b4c
Hyd3dF <max825420@gmail.com>
2026-09-03 20:56:56 +0300

    Delete backend directory

--- backend/internal/api/users_routes.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "net/http"
-
-func (s *Server) registerUserRoutes(mux *http.ServeMux, limits routeLimiters) {
-	mux.Handle("GET /v1/users/search", s.authenticated(limits.read, "read", s.searchUsers))
-}
